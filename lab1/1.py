be5fe69efe7a5c627b3a0a82da583d39a3450e0c
Artem-Booldyr <boldyr.art@gmail.com>
2025-10-09 11:12:34 +0300

    1

---
+++ lab1/1.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+name = "Artem"
+location = "Lviv"
+
+print(f"{name} start programming at {datetime.now()}. {location} is the best city!")
---
+++ lab1/copilot.py
@@ -0,0 +1,12 @@
+a = 5
+b = 3
+
+sum_result = a + b
+diff_result = a - b
+prod_result = a * b
+div_result = a / b
+
+print("Сума:", sum_result)
+print("Різниця:", diff_result)
+print("Добуток:", prod_result)
+print("Ділення:", div_result)
